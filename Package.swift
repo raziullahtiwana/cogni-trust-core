998ca35c160f7beb0038ac5538047a04a630750a
Razi Tiwana <mraziullah111@gmail.com>
2022-11-20 13:24:59 +0500

    Add SwiftProtobuf as dependency

--- Package.swift
+++ Package.swift
@@ -6,19 +6,18 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobufCore", targets: ["SwiftProtobufCore"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+              name: "SwiftProtobuf",
+              url: "https://github.com/apple/swift-protobuf.git",
+              "1.15.0" ..< "2.0.0"
+            ),],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.0/WalletCore.xcframework.zip",
             checksum: "2487af30a8f6f4775a41f29456f792f15269f4f4daae2da7e4b9ef8747613e3a"
-        ),
-        .binaryTarget(
-            name: "SwiftProtobufCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.0/SwiftProtobuf.xcframework.zip",
-            checksum: "2038b1d43c9f6aeb4957e3763382b8558983a1a811b168dca71be3636bb8350a"
         )
     ]
 )
