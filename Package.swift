404a4c047136289ee8dad7890a37048c29360592
Razi Tiwana <razi.tiwana@cogni-ext.com>
2022-11-22 15:43:33 +0500

    SwiftProtobuf added

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
+        .library(name: "SwiftProtobufWalletCore", targets: ["SwiftProtobuf"]),
     ],
-    dependencies: [
-        .package(
-              name: "SwiftProtobuf",
-              url: "https://github.com/apple/swift-protobuf.git",
-              "1.15.0" ..< "2.0.0"
-            ),],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.0/WalletCore.xcframework.zip",
             checksum: "2487af30a8f6f4775a41f29456f792f15269f4f4daae2da7e4b9ef8747613e3a"
+        ),
+        .binaryTarget(
+            name: "SwiftProtobuf",
+            path:"SwiftProtobuf"
         )
     ]
 )
